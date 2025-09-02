577598019042478f893dd9178ef07b79ed1cbeab
VortexStecu <ranxgaming48@gmail.com>
2025-09-02 14:52:33 +0700

    Update AIRPROXY.lua

--- AIRPROXY.lua
+++ AIRPROXY.lua
@@ -1,14 +1,29 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
-	Name = "RIVALS SCRIPT | MADE BY VORSTECU",
-	LoadingTitle = "Loading...",
-	LoadingSubtitle = "SSSIIUUUUU",
-	ConfigurationSaving = {
-		Enabled = false,
-		FolderName = nil,
-		FileName = "Rivals Script"
-	},
+   Name = "Rivals | BY VorStecu Hub",
+   LoadingTitle = "VortexStecu Hub",
+   LoadingSubtitle = "by VorStecu",
+   ConfigurationSaving = {
+      Enabled = false,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "VortexStecu Hub"
+   },
+   Discord = {
+      Enabled = false,
+      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+   KeySystem = true, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Key | Youtube Hub",
+      Subtitle = "Key System",
+      Note = "Key In Discord Server",
+      FileName = "YoutubeHubKey1", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = false, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"VortexStecu"} -- List of keys that will be accepted by the system, can b file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
 })
 
 local Config = {
